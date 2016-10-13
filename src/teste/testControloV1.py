f16f0bd0b5f3bd5da14c12ad623515f1cfb27d86
Trinkes <fabiojscosta1583@gmail.com>
2016-10-13 01:50:26 +0100

    created a module to take care of the algorithms implementations

--- src/teste/testControloV1.py
+++ src/teste/testControloV1.py
@@ -1,8 +1,8 @@
 import psa
 
 from src.AgenteProspector.agenteProspector import AgenteProspector
+from src.algorithmImp.moveToTargetV1 import MoveToTargetV1
 from src.controllerImp.controloV1 import ControloV1
-from src.controllerImp.moveToTargetV1 import MoveToTargetV1
 
 psa.iniciar("../amb/amb2.das")
 
